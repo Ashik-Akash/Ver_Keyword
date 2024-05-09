91200a21379a222ca0bc479a9484d129553260a3
Ashik-Akash <ashikikbalakash15@gmail.com>
2024-05-10 00:00:30 +0600

    first commit

---
+++ VerKeyword/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Ashik";
+            var age = 10;
+            var number = "0163839494";
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(number);
+
+           Console.ReadLine();
+
+        }
+    }
+}
